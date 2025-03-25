5be24bdb2c1d8dbf5a330d247a3eeeaf0a3372db
kimdongheecom <dongheekim@gmail.com>
2025-03-25 16:40:10 +0900

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
